43d430e5f85e83ab00e068554360c844b2c6e915
Roger <Roger.Richards07@gmail.com>
2020-05-29 23:00:43 -0400

    gradle update

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -1,9 +1,31 @@
 package com.galvanize;
 
+import java.util.Scanner;
+
 public class Application {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static boolean isActive(String status) {
+        // Implement your solution
+
+        if (status.equals("active")) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
+    public static void main(String[] args) {
+
+        String status;
+
+        do
+        {
+            System.out.println("Please enter a status that literally says,'active':");
+            Scanner in = new Scanner(System.in);
+            status = in.next();
+        } while ( !isActive(status) );
+        System.out.println("Thank you.");
+
+    }
 }
