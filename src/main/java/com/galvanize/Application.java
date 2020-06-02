b0ddeb317877dd31b93d8827510493b41f62c39e
Roger <Roger.Richards07@gmail.com>
2020-06-02 18:43:02 -0400

    git update

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -1,31 +1,16 @@
 package com.galvanize;
 
-import java.util.Scanner;
 
 public class Application {
 
-    public static boolean isActive(String status) {
-        // Implement your solution
 
-        if (status.equals("active")) {
-            return true;
-        } else {
-            return false;
-        }
-
+    public static void main(String... args) {
+    if(args.length < 1){
+        System.out.println("Please provide an argument");
+    }
+    else {
+        System.out.println(String.format("Argument provided: %s", args[0]));
+    }
     }
 
-    public static void main(String[] args) {
-
-        String status;
-
-        do
-        {
-            System.out.println("Please enter a status that literally says,'active':");
-            Scanner in = new Scanner(System.in);
-            status = in.next();
-        } while ( !isActive(status) );
-        System.out.println("Thank you.");
-
-    }
 }
